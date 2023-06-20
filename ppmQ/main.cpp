0a58fb220e7cdebe6885bed1eea4610b1d22d103
caesiumCode <caesiumcode@aol.com>
2023-06-20 23:15:35 +1000

    add limit

--- ppmQ/main.cpp
+++ ppmQ/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, const char * argv[])
     
     std::vector<uint8_t> orders = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     //std::vector<uint32_t> max_contexts = {1000, 10000, 100000, 1000000, 10000000, std::numeric_limits<uint32_t>::max()};
-    std::vector<uint32_t> max_contexts = {1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000};
+    std::vector<uint32_t> max_contexts = {1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, std::numeric_limits<uint32_t>::max()};
     std::vector<uint32_t> mults = {2, 3, 5, 8};
     
     
