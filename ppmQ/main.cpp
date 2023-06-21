ae23687d473ddcaff8acbe5f023066088983bdef
caesiumCode <caesiumcode@aol.com>
2023-06-21 11:18:14 +1000

    add factor

--- ppmQ/main.cpp
+++ ppmQ/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, const char * argv[])
     std::vector<uint8_t> orders = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     //std::vector<uint32_t> max_contexts = {1000, 10000, 100000, 1000000, 10000000, std::numeric_limits<uint32_t>::max()};
     std::vector<uint32_t> max_contexts = {1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, std::numeric_limits<uint32_t>::max()};
-    std::vector<uint32_t> mults = {2, 3, 5, 8};
+    std::vector<uint32_t> mults = {1, 2, 3, 5, 8};
     
     
     for (uint8_t order : orders) for (uint32_t max_context : max_contexts) for (uint32_t mult : mults)
